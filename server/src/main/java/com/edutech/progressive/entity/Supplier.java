da7e3f97f43c66c4334c362254150ce4c67c837f
Charu240903 <dummy@gmail.com>
2026-01-22 04:03:13 +0000

    Day 6

--- server/src/main/java/com/edutech/progressive/entity/Supplier.java
+++ server/src/main/java/com/edutech/progressive/entity/Supplier.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Supplier implements Comparable<Supplier> {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int supplierId;
